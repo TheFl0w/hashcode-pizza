771e4204b9cda3b3eb33ac94347676502b4431c9
TheFl0w <florian.warg@hotmail.de>
2019-02-26 20:19:40 +0100

    T=0, M=1

--- src/io.cpp
+++ src/io.cpp
@@ -13,6 +13,10 @@ void read_input(std::istream& is) {
     for (int i = 0; i < r; ++i) {
         for (int j = 0; j < c; ++j) {
             is >> mat[i * c + j];
+            if (mat[i * c + j] == 'T')
+                mat[i * c + j] = 0;
+            else
+                mat[i * c + j] = 1;
         }
         is.get();
     }
