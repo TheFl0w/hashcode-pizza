e4bd2dffd0b7dd7b4c0bf4554afa0e4deaa0c87a
TheFl0w <florian.warg@hotmail.de>
2019-02-26 20:00:51 +0100

    update io api

--- include/pizza.hpp
+++ include/pizza.hpp
@@ -1,11 +1,14 @@
 #pragma once
+
 #include <istream>
+#include <vector>
 
 struct Input {
     int r;
     int c;
     int l;
     int h;
+    std::vector<char> matrix;
 };
 
 Input read_input(std::istream& is);
